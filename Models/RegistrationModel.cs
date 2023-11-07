d29ab9d6a48257824f8f9bb18511a3aabe2eb22a
Keshav Raj Choudhary <Keshavrajchaudhary@t-a-g.net>
2023-11-07 23:31:44 +0530

    commiting changes for payment

---
+++ Models/RegistrationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace user_service.Models
+{
+    public class RegistrationModel
+    {
+        public string Name { get; set; } = "keshav";
+        public string Mobile { get; set; } = "8210093342";
+        public string Email { get; set; } = "keshavraj123.kr@gmail.com";
+        public decimal Amount { get; set; } = 100;
+    }
+}
