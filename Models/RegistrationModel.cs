bf3fd3c97876b7dfb5f047b9b763927e643b30ec
Keshav Raj Choudhary <Keshavrajchaudhary@t-a-g.net>
2023-11-09 07:35:44 +0530

    final commit before preview don't change

--- Models/RegistrationModel.cs
+++ Models/RegistrationModel.cs
@@ -2,9 +2,9 @@
 {
     public class RegistrationModel
     {
-        public string Name { get; set; } = "keshav";
-        public string Mobile { get; set; } = "8210093342";
-        public string Email { get; set; } = "keshavraj123.kr@gmail.com";
-        public decimal Amount { get; set; } = 100;
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public decimal Amount { get; set; }
     }
 }
